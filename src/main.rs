13f4e48169bac7b1ce7265b9553502a5d3f31432
Keisuke Ota <k.ota109@outlook.com>
2024-10-18 01:27:37 +0900

    S3からファイルを取得する処理を実装中(ローカルで動かない)

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,20 @@
 mod error;
+mod s3;
 
-use aws_config::meta::region::RegionProviderChain;
+use serde_json::Value;
+use std::{collections::HashMap, path};
+
 use lambda_runtime::{service_fn, Error, LambdaEvent};
-use serde_json::{Value, json};
-
-use std::{fs::File, io::{self, Write}, path::PathBuf, process::exit};
-use aws_sdk_s3::{Client, Config};
-use clap::Parser;
-use tracing::trace;
-
-#[derive(Debug, Parser)]
-struct Opt {
-    #[structopt(long)]
-    bucket: String,
-    #[structopt(long)]
-    object: String,
-    #[structopt(long)]
-    destination: PathBuf,
-}
-
-async fn get_object(client: Client, opt: Opt) -> Result<usize, aws_sdk_s3::Error> {
-    trace!("bucket:      {}", opt.bucket);
-    trace!("object:      {}", opt.object);
-    trace!("destination: {}", opt.destination.display());
-
-    let mut file = File::create(opt.destination.clone())?;
-
-    let mut object = client
-        .get_object()
-        .bucket(opt.bucket)
-        .key(opt.object)
-        .send()
-        .await?;
-
-    let mut byte_count = 0_usize;
-    while let Some(bytes) = object.body.try_next().await? {
-        let bytes_len = bytes.len();
-        file.write_all(&bytes)?;
-        trace!("Intermediate write of {bytes_len}");
-        byte_count += bytes_len;
-    }
-
-    Ok(byte_count)
-}
 
 async fn handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
-    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
-    let config = aws_config::from_env().region(region_provider).load().await;
-    let client = Client::new(&config);
-    
-    match get_object(client, Opt::parse()).await {
-        Ok(bytes) => {
-            println!("Wrote {bytes}");
-        }
-        Err(err) => {
-            eprintln!("Error: {}", err);
-            exit(1);
-        }
-    }
-    let response_value = json!({
-        "status": "success"
-    });
+    let contents = s3::get_object(&"sample_word_document.docx".to_string()).await?;
 
-    // 成功時の戻り値を返す
-    Ok(response_value)
+    let json_contents = match serde_json::from_slice(&contents) {
+        Ok(val) => val,
+        Err(_) => serde_json::json!({"error": "Invalid Json"}),
+    };
+
+    Ok(json_contents)
 }
 
 #[tokio::main]
--- src/s3.rs
+++ src/s3.rs
@@ -1,41 +1,41 @@
-use aws_config::meta::region::RegionProviderChain;
-use std::{fs::File, io::{self, Write}, path::PathBuf, process::exit};
+use crate::error::S3Error;
 
-use aws_sdk_s3::{Client, Config};
-use clap::Parser;
-use tracing::trace;
+use aws_config::{
+    default_provider::credentials, meta::region::RegionProviderChain,
+    profile::ProfileFileCredentialsProvider,
+};
+use aws_sdk_s3::{config::Region, Client};
 
-#[derive(Debug, Parser)]
-struct Opt {
-    #[structopt(long)]
-    bucket: String,
-    #[structopt(long)]
-    object: String,
-    #[structopt(long)]
-    destination: PathBuf,
-}
+pub async fn get_object(path: &String) -> Result<Vec<u8>, S3Error> {
+    let bucket = "test-bucket";
+    let region = "ap-northeast-1";
 
-async fn get_object(client: Client, opt: Opt) -> Result<usize, io::Error> {
-    trace!("bucket:      {}", opt.bucket);
-    trace!("object:      {}", opt.object);
-    trace!("destination: {}", opt.destination.display());
+    let region_provider =
+        RegionProviderChain::first_try(Region::new(region.clone())).or_else("ap-northeast-1");
 
-    let mut file = File::create(opt.destination.clone())?;
+    // ローカルのminioに対して実行するときに必要
+    let credentials = ProfileFileCredentialsProvider::builder()
+        .profile_name("default")
+        .build();
+    let config = aws_config::from_env()
+        .endpoint_url("http://localhost:55510")
+        .credentials_provider(credentials)
+        .region(region_provider)
+        .load()
+        .await;
 
-    let mut object = client
-        .get_object()
-        .bucket(opt.bucket)
-        .key(opt.object)
-        .send()
-        .await?;
-    let mut byte_count = 0_usize;
-    while let Some(bytes) = object.body.try_next().await? {
-        let bytes_len = bytes.len();
-        file.write_all(&bytes)?;
-        trace!("Intermediate write of {bytes_len}");
-        byte_count += bytes_len;
+    let client = Client::new(&config);
+
+    let mut object = client.get_object().bucket(bucket).key(path).send().await?;
+
+    let mut byte_data = Vec::new();
+    while let Some(bytes) =
+        object.body.try_next().await.map_err(|err| {
+            S3Error::new(format!("Failed to read from S3 download stream: {err:?}"))
+        })?
+    {
+        byte_data.extend_from_slice(&bytes);
     }
 
-    Ok(byte_count)
+    Ok(byte_data)
 }
-
